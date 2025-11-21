987a4ae5accd06af40b06352e733cd2dd05d2138
Sohil876 <sohil876@gmail.com>
2025-11-21 22:50:48 +0530

    Revert "Implement manual session support, only show/load linting of current in focus file"
    This reverts commit e757c5a79f3902b7177f968703fe776f15e8afd5.

--- src/main.js
+++ src/main.js
@@ -2,21 +2,12 @@ import { LanguageProvider } from "ace-linters/build/ace-linters";
 import plugin from "../plugin.json";
 
 class AcodeAceLinters {
-	constructor() {
-		this.onSwitchFile = this.onSwitchFile.bind(this);
-		this.lastSession = null;
-	}
-
 	async init() {
 		const workerUrl = this.baseUrl + "worker.js";
 
 		this.worker = new Worker(workerUrl);
-		this.provider = LanguageProvider.create(this.worker, {
-			manualSessionControl: true,
-		});
+		this.provider = LanguageProvider.create(this.worker);
 		this.provider.registerEditor(editorManager.editor);
-
-		editorManager.on("switch-file", this.onSwitchFile);
 
 		const extensions = [
 			"json",
@@ -35,42 +26,20 @@ class AcodeAceLinters {
 		acode.registerFormatter(plugin.id, extensions, () => {
 			this.provider.format();
 		});
-		this.onSwitchFile();
+
 		window.toast("Ace Linters: Loaded successfully!");
 	}
 
-	onSwitchFile() {
-		const { editor } = editorManager;
-		const session = editor.session;
-		const file = editorManager.activeFile;
-
-		// Safety checks
-		if (!session || !file) return;
-
-		if (this.lastSession && this.lastSession !== session) {
-			this.lastSession.clearAnnotations();
-			this.lastSession.clearBreakpoints();
-			this.provider.closeDocument(this.lastSession);
-		}
-
-		const options = {
-			filePath: file.filename || "unknown.txt",
-			joinWorkspaceURI: false,
-		};
-		this.provider.registerSession(session, editor, options);
-		this.lastSession = session;
-	}
-
 	async destroy() {
-		editorManager.off("switch-file", this.onSwitchFile);
 		try {
 			acode.unregisterFormatter(plugin.id);
 		} catch (e) {}
 
 		try {
-			if (this.lastSession) {
-				this.lastSession.clearAnnotations();
-				this.lastSession.clearBreakpoints();
+			if (editorManager.editor && editorManager.editor.session) {
+				const session = editorManager.editor.session;
+				session.clearAnnotations();
+				session.clearBreakpoints();
 				editorManager.editor.renderer.updateBackMarkers();
 			}
 		} catch (e) {
